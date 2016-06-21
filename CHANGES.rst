1e625c54b4073c8d5dc8314bad1f7f32007f2938
Jason R. Coombs <jaraco@jaraco.com>
2016-06-21 12:41:12 -0400

    Add Pytest trove classifier

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,3 +1,9 @@
+2.9
+~~~
+
+* Added Trove Classifier indicating this package is part
+  of the pytest framework.
+
 2.8
 ~~~
 
--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup_params = dict(
 		"Programming Language :: Python :: 2.6",
 		"Programming Language :: Python :: 2.7",
 		"Programming Language :: Python :: 3",
+		"Framework :: Pytest",
 	],
 	entry_points = {
 		'distutils.commands': [
